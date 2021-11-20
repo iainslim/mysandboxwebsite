342ec31aa9fbe6aa8d6877e63bebb59392f8711d
Iain Slim <iain.j.slim@outlook.com>
2021-11-19 22:27:20 -0500

    finished DOM 2 video

--- dom.js
+++ dom.js
@@ -46,16 +46,72 @@
 // }
 
 // QUERYSELECTOR //
-var input = document.querySelector('input');
-input.value = 'Hello World';
+// var input = document.querySelector('input');
+// input.value = 'Hello World';
 
-// QUERYSELECTORALL //
-var odd = document.querySelectorAll('li:nth-child(odd)');
-var even = document.querySelectorAll('li:nth-child(even)');
+// // QUERYSELECTORALL //
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// var even = document.querySelectorAll('li:nth-child(even)');
 
-for(var i=0; i <odd.length; i++){
-    odd[i].style.backgroundColor = '#f4f4f4';
-    even[i].style.backgroundColor = '#ccc';
-}
+// for(var i=0; i <odd.length; i++){
+//     odd[i].style.backgroundColor = '#f4f4f4';
+//     even[i].style.backgroundColor = '#ccc';
+// }
+
+// Traversing the DOM //
+var itemList = document.querySelector('#miscthings');
+//parentNode
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentNode.parentNode);
+
+// //parentElement
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentElement.parentElement);
+
+// //childNodes
+// console.log(itemList.childNodes);
 
 
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style. backgroundColor = 'red';
+
+// //FirstChild
+// console.log(itemList.firstChild); //gives #text (usually kind of useless)
+// //FirstElementChild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'Hello there friend';
+
+
+// //nextSibling
+// console.log(itemList.nextSibling)
+// //nextElementSibling
+// console.log(itemList.nextElementSibling);
+
+// //previousSibling
+// console.log(itemList.previousSibling);
+// //previousElementSibling
+// console.log(itemList.previousElementSibling);
+
+// createElement
+
+//Creating a div
+var newDiv = document.createElement('div');
+
+newDiv.className = 'hello';
+newDiv.id = 'hello-friend';
+newDiv.setAttribute('title', 'Hello div');
+
+var newDivText = document.createTextNode('Some Text for the div');
+
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+
+console.log(newDiv);
+
+container.insertBefore(newDiv, h1);
